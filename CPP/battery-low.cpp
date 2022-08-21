de12ff507461a4fc665931b698d3228f0cd383ef
Abhinav Saxena <abhinav150601@gmail.com>
2022-08-21 18:40:13 +0530

    Day 2

---
+++ CPP/battery-low.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,x;
+    cin >> n;
+    while(n--)
+    {
+        cin >>x;
+        if(x<=15)
+        {
+            cout<<"Yes"<<endl;
+        }
+        else
+        {
+            cout<<"No"<<endl;
+        }
+    }
+    return 0;
+}
