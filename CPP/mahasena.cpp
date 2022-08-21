b07fb928e44288419ff63c5016c86c7e2b0ce12d
Abhinav Saxena <abhinav150601@gmail.com>
2022-08-21 19:48:17 +0530

    Day 2

---
+++ CPP/mahasena.cpp
@@ -0,0 +1,36 @@
+//Problem Code: AMR15A
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x,y;
+    int counnter_even=0;
+    int counter_odd=0;
+    cin >> x;
+    while(x--)
+    {
+        cin >>y;
+        if(y%2==0)
+        {
+            counnter_even = counnter_even+1;
+            //cout << y;
+            
+        }
+        else
+        {
+            counter_odd = counter_odd+1;
+            //cout <<y<<"odd";
+        }
+    }
+    if(counnter_even>counter_odd)
+        {
+            cout<<"READY FOR BATTLE";
+        
+        }
+    else if (counnter_even<=counter_odd)
+        {
+            cout<<"NOT READY";
+        }
+        return 0;
+}
