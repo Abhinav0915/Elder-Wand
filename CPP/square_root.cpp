e3621540539529c0b2448202ab0438195820ed4b
Abhinav Saxena <abhinav150601@gmail.com>
2022-08-22 20:27:45 +0530

    Day 3

---
+++ CPP/square_root.cpp
@@ -0,0 +1,17 @@
+// Problem Code:FSQRT
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    while(x--)
+    {
+        int y;
+        cin >>y;
+
+        int s_root=sqrt(y);
+        cout << s_root << endl;
+    }    
+    return 0;
+}
