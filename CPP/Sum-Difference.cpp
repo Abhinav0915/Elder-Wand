7b304ff287616380fd01c3692ac3cf9047fe7ee1
Abhinav Saxena <abhinav150601@gmail.com>
2022-08-26 18:59:09 +0530

    Day 6

---
+++ CPP/Sum-Difference.cpp
@@ -0,0 +1,17 @@
+//Problem Code: DIFFSUM
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,x;
+    cin>>n>>x;
+    if(n>x)
+    {
+        cout<<n-x<<endl;
+    }
+    else
+    {
+        cout<<x+n<<endl;
+    }
+}
